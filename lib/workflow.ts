916563d0c30291247fb076b52b76bcb7bebde83f
Chirag Jain <chiragjain7300@gmail.com>
2025-06-07 00:25:00 +0530

    Typo Change

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -26,7 +26,7 @@ export const sendEmail = async ({
       provider: resend({ token: config.env.resendToken }),
     },
     body: {
-      from: "Chirag Jain <onboarding@resend.dev>",
+      from: "Acme <onboarding@resend.dev>",
       to: [email],
       subject,
       html: message,
